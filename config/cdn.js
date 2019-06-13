834bc6c26e3e9bf582f669345915c15e5cf8fa1c
苏芳儒 <sufangru@baibu.la>
2019-06-13 15:57:08 +0800

    chore: 升级 vant-ui 版本
    (cherry picked from commit a740293968b95bf5e607e1c94c153bb44ac1dd12)

--- config/cdn.js
+++ config/cdn.js
@@ -3,14 +3,14 @@
  */
 module.exports = {
   css: [
-    '//cdn.jsdelivr.net/npm/vant@1.6/lib/index.css',
+    '//cdn.jsdelivr.net/npm/vant@2.0/lib/index.css',
     '//cdn.bootcss.com/nprogress/0.2.0/nprogress.min.css',
   ],
   js: [
     '//cdn.bootcss.com/vue/2.5.21/vue.min.js',
     '//cdn.bootcss.com/vue-router/3.0.1/vue-router.min.js',
     '//cdn.bootcss.com/vuex/3.0.1/vuex.min.js',
-    '//cdn.jsdelivr.net/npm/vant@1.6/lib/vant.min.js',
+    '//cdn.jsdelivr.net/npm/vant@2.0/lib/vant.min.js',
     '//cdn.bootcss.com/axios/0.18.0/axios.min.js',
     '//cdn.bootcss.com/js-cookie/2.2.0/js.cookie.min.js',
     '//cdn.bootcss.com/nprogress/0.2.0/nprogress.min.js',
